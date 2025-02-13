4a2b7822922aa2da210888b25535910703dd5b41
Amal Nisha <amalnisha7705@gmail.com>
2025-02-13 10:40:14 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+<footer class="footer">
+    <div class="footer-container">
+        <p>© 2025 Your Name. All Rights Reserved.</p>
+        <div class="footer-links">
+            <a href="#">Privacy Policy</a>
+            <a href="#">Terms of Service</a>
+            <a href="#">Contact</a>
+        </div>
+        <div class="footer-social">
+            <a href="#"><i class="fab fa-github"></i></a>
+            <a href="#"><i class="fab fa-linkedin"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+        </div>
+    </div>
+</footer>
